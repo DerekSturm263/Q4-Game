05ffa4abe65498361b5ba4f51864104993d1540f
JaredNo <jar091002@yahoo.com>
2021-04-16 13:19:53 -0700

    Time display

---
+++ Assets/Resources/Scripts/UIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject timeDisplay;
+
+    public float time;
+    public float cycleLength;
+    private float cycleSeconds;
+    private Quaternion timeDisplayRoation;
+
+    void Start()
+    {
+        cycleSeconds = cycleLength * 60;
+        timeDisplayRoation = Quaternion.identity;
+    }
+    
+    void Update()
+    {
+        Vector3 rotationVector = new Vector3(0, 0, -((time / cycleSeconds) * 360));
+        timeDisplayRoation.eulerAngles = rotationVector;
+
+        timeDisplay.transform.rotation = timeDisplayRoation;
+        //Debug.Log(timeDisplayRoation.eulerAngles);
+    }
+}
