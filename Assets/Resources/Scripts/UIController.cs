0676fc7f0a2e43d11408686f6af46f3959be3cb9
JaredNo <jar091002@yahoo.com>
2021-04-20 16:24:22 -0700

    String idicator for times of day (dawn, day, etc), Rotation resets at 360
    The exact rotation of when the different times of day are can be tweaked easily however I feel as though it makes sense in its current form

--- Assets/Resources/Scripts/UIController.cs
+++ Assets/Resources/Scripts/UIController.cs
@@ -7,6 +7,7 @@ public class UIController : MonoBehaviour
     public GameObject timeDisplay;
 
     public static float time;
+    public static string timeTitle;
     public float cycleLength;
     private float cycleSeconds;
     private Quaternion timeDisplayRoation;
@@ -17,13 +18,33 @@ public class UIController : MonoBehaviour
         timeDisplayRoation = Quaternion.identity;
     }
     
-    void Update()
+    void FixedUpdate()
     {
         time += Time.deltaTime;
-        Vector3 rotationVector = new Vector3(0, 0, -((time / cycleSeconds) * 360));
+        float rotCalc = -((time / cycleSeconds) * 360);
+        if (rotCalc <= -360)
+            time = 0;
+        Vector3 rotationVector = new Vector3(0, 0, rotCalc);
         timeDisplayRoation.eulerAngles = rotationVector;
 
         timeDisplay.transform.rotation = timeDisplayRoation;
+
+        if ((int)rotCalc == -180)
+        {
+            timeTitle = "night";
+        } else if ((int)rotCalc == -150)
+        {
+            timeTitle = "dusk";
+        } else if ((int)rotCalc == -30)
+        {
+            timeTitle = "day";
+        } else if ((int)rotCalc == 0)
+        {
+            timeTitle = "dawn";
+        }
+
+        //Debug.Log((int)rotCalc + ": " + timeTitle);
+        //Debug.Log(timeTitle);
         //Debug.Log(timeDisplayRoation.eulerAngles);
     }
 }
