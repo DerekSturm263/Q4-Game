65a3c2954a99e4f40b72856d44bcaa7d331c7c49
JaredNo <jar091002@yahoo.com>
2021-04-22 18:35:55 -0700

    Berry bag with working add/take food

--- Assets/Resources/Scripts/UIController.cs
+++ Assets/Resources/Scripts/UIController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class UIController : MonoBehaviour
 {
     public GameObject timeDisplay;
+    public GameObject berryPrefab;
 
     public static float time;
     public static string timeTitle;
     public float cycleLength;
     private float cycleSeconds;
     private Quaternion timeDisplayRoation;
+
+    public static float numFood;
 
     void Start()
     {
@@ -29,16 +32,18 @@ public class UIController : MonoBehaviour
 
         timeDisplay.transform.rotation = timeDisplayRoation;
 
-        if ((int)rotCalc == -180)
+        if ((int)rotCalc == -180 && timeTitle != "night")
         {
             timeTitle = "night";
-        } else if ((int)rotCalc == -150)
+        } else if ((int)rotCalc == -150 && timeTitle != "dusk")
         {
             timeTitle = "dusk";
-        } else if ((int)rotCalc == -30)
+            TakeFood(2); // Food is taken away every day at dusk
+        } else if ((int)rotCalc == -30 && timeTitle != "day")
         {
             timeTitle = "day";
-        } else if ((int)rotCalc == 0)
+            GiveFood(2); //Simulating the player finding food
+        } else if ((int)rotCalc == 0 && timeTitle != "dawn")
         {
             timeTitle = "dawn";
         }
@@ -47,4 +52,34 @@ public class UIController : MonoBehaviour
         //Debug.Log(timeTitle);
         //Debug.Log(timeDisplayRoation.eulerAngles);
     }
+
+    public void GiveFood(int numFoodToGive)
+    {
+        for (int i = 0; i < numFoodToGive; i++)
+        {
+            numFood++;
+            Instantiate(berryPrefab, new Vector3(-2.5f, 10, 0), Quaternion.identity);
+        }
+
+        Debug.Log(numFood);
+    }
+
+    public void TakeFood(int numFoodToTake)
+    {
+        GameObject[] berries;
+        berries = GameObject.FindGameObjectsWithTag("UIBerry");
+
+        Debug.Log("Berries Found: " + berries.Length);
+
+        for (int i = 0; i < numFoodToTake; i++)
+        {
+            if (numFood == 0)
+                break;
+            berries[berries.Length - 1 - i].GetComponent<Rigidbody2D>().gravityScale = -1;
+            numFood--;
+            Debug.Log(numFood);
+        }
+    }
+
+   
 }
