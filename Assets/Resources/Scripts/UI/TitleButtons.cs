cbb0627815ffc108b8b44a015e1ec4b87fe2d163
Derek Sturm <dereksturm263@gmail.com>
2021-05-25 22:03:04 -0700

    oop

--- Assets/Resources/Scripts/UI/TitleButtons.cs
+++ Assets/Resources/Scripts/UI/TitleButtons.cs
@@ -24,13 +24,13 @@ public class TitleButtons : MonoBehaviour
 
             MusicPlayer.Play(0, "Lost in the Woods (Main Theme)");
             MusicPlayer.Play(1, "Lost in the Ocean (Underwater Theme)");
-            MusicPlayer.SetVolume(0, 0.5f);
+            MusicPlayer.SetVolume(0, 1f);
             MusicPlayer.SetVolume(1, 0f);
 
             SoundPlayer.SetVolume(0.5f);
 
             GameController.musicVolume = MusicPlayer.volume[0];
-            GameController.musicVolume2 = MusicPlayer.volume[0] * 0.3f;
+            GameController.musicVolume2 = MusicPlayer.volume[0] * 0.6f;
         }
 
         loadGameButton.SetActive(SaveDataController.HasSave());
